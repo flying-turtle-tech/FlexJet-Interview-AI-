30b2ec249f6b9f70acb2a637447410eb9f34b5d4
Jonathan <jonny.k242@gmail.com>
2026-01-18 10:06:27 -0500

    Initial Commit

---
+++ FlexJetting/FlexJettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexJettingApp.swift
+//  FlexJetting
+//
+//  Created by Jonathan on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct FlexJettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
